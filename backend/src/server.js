2a88dd7f79b300c787e1ed14d4d9770d73080678
Devendrak08 <devendrakkumar08@gmail.com>
2026-07-25 03:14:32 +0530

    chore: configure deployment settings

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 import { serve } from "inngest/express";
+import path from "path";
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./config/db.js";
@@ -10,16 +11,30 @@ const app = express();
 
 const PORT = ENV.PORT || 5001;
 
+const __dirname = path.resolve();
+
 //middleware
 app.use(express.json());
 // credentials true meaning >> server allows a browser to  include cookies on request
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+// app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
 app.use("/api/inngest", serve({ client: inngest, functions }))
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Success runnning from api" })
+app.get("/books", (_, res) => {
+  res.status(200).json({ message: "API is working" })
 })
+app.get("/health", (_, res) => {
+  res.status(200).json({ message: "API is working" })
+})
+
+// deployment config
+if (ENV.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 
 const startServer = async () => {
