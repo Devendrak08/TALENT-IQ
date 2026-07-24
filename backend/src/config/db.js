5b7db26fa3c5ae91a7e5bd7f3eda4429b6e5c14d
Devendrak08 <devendrakkumar08@gmail.com>
2026-07-25 01:45:34 +0530

    chore(backend): validate MongoDB URI environment variable

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,6 +5,9 @@ import { ENV } from "../lib/env.js";
 
 export const connectDB = async() => {
     try {
+        if(!ENV.MONGO_URI) {
+            throw new error("MONGO_URI is not available in environment variables");
+        }
         const conn = await mongoose.connect(ENV.MONGO_URI);
         console.log("Database connected successfully", conn.connection.host);
     } catch (error) {
