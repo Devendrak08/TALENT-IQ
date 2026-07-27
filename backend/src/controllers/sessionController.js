70d0a04b7470ddf8a0eb0b746d97b5a686409492
Devendrak08 <devendrakkumar08@gmail.com>
2026-07-28 05:15:20 +0530

    quick changes

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -157,7 +157,7 @@ export async function endSession(req, res) {
     res.status(200).json({ session, message: "Session ended successfully" });
 
   } catch (error) {
-    console.error("Error in endSession controller::", error);
-    res.status(500).json({ message: "Internal Server Error" })
+    conselo.error("Error in endSession controller::", error);
+    res.status(400).json({ message: "Internal Server Error" })
   }
 }
